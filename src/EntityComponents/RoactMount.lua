4826a22c4161ec3c08bd2f82852ba39021b2b931
Ken Barber <ken@bob.sh>
2018-06-11 19:56:42 +0100

    New RoactMount ECS component to bridge these two worlds

---
+++ src/EntityComponents/RoactMount.lua
@@ -0,0 +1,18 @@
+--[[--
+    Mount a Roact app to the Components ScreenGui instance.
+    @module RoactMount
+--]]
+
+local EntitySystem = require(game.ReplicatedStorage.EntitySystem)
+local Roact = require(game.ReplicatedStorage.Modules.Roact)
+
+local RoactMount = EntitySystem.Component:extend("RoactMount", {
+    App = "Demo",
+})
+
+function RoactMount:added()
+    local app = require(game.ReplicatedStorage.BTK.RoactComponents[self.App])
+    Roact.mount(app, self.instance)
+end
+
+return RoactMount
